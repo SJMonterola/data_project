995cef57c1350578105d77661adb70447767ed4a
Segern <smonterola2@academic.rrc.ca>
2025-03-22 15:59:15 -0500

    Made it so the 'View More' text below the random facts on the homepage leads to the show page of that fact

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    # For displaying the random facts on start up
     @cat_fact = CatFact.find(CatFact.ids.sample)
     @dog_fact = DogFact.find(DogFact.ids.sample)
   end
