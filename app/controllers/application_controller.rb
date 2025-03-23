7a2f4618098bdc409de9f54b465a2a16b036ba61
Segern <smonterola2@academic.rrc.ca>
2025-03-23 13:36:01 -0500

    Cleaned up the HTML a bit by making some additions and getting rid of unused tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  # For the search bar
-  # @query = params[:query]
-  # @authors = Author.where("name LIKE ?", "%#{@query}")
-  # @cat_facts = CatFact.where("name LIKE ?", "%#{@query}")
-  # @dog_facts = DogFact.where("name LIKE ?", "%#{@query}")
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,5 @@ class HomeController < ApplicationController
     # For displaying the random facts on start up
     @cat_fact = CatFact.find(CatFact.ids.sample)
     @dog_fact = DogFact.find(DogFact.ids.sample)
-    # For the search bar (cannot decide whether it goes in here or in app controller)
-    # @query = params[:query]
-    # @authors = Author.where("name LIKE ?", "%#{@query}")
-    # @cat_facts = CatFact.where("name LIKE ?", "%#{@query}")
-    # @dog_facts = DogFact.where("name LIKE ?", "%#{@query}")
   end
 end
